701211e3d570086de9c59373263fcf0866b3b4d1
amacshines <nekromanter20000@gmail.com>
2016-12-05 11:52:23 +0300

    init

---
+++ src/CurrencyApbParser.cs
@@ -0,0 +1,94 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CurrencyAPB
+{
+    public class CurrencyApbParser
+    {
+        RestClient client;
+        RestRequest request;
+        private string adressUrl = "https://www.agroprombank.com/xmlinformer.php";
+        public CurrencyApbParser(string url = null)
+        {
+            if (!string.IsNullOrEmpty(url))
+                adressUrl = url;
+            client = new RestClient(adressUrl);            
+            request = new RestRequest("", Method.GET);
+        }
+
+        /// <summary>
+        /// Возвращает все курсы валют на сегодня
+        /// </summary>
+        /// <returns></returns>
+        public Courses GetAllToday()
+        {
+            var response = client.Execute<Currency>(request);
+            var inputString = response.Content.Replace("<?xml version=\"1.0\" encoding=\"windows-1251\"?>", "");
+            XmlSerializer serializer = new XmlSerializer(typeof(Courses));
+            var result = new Courses();
+            using (TextReader reader = new StringReader(inputString))
+            {
+                result = (Courses)serializer.Deserialize(reader);                
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Возвращает все курсы валют за определённую дату (YYYY-MM-DD)
+        /// </summary>
+        /// <returns></returns>
+        public Courses GetAllByDate(DateTime date)
+        {
+            var formatDate = date.ToString("yyyy-MM-dd");
+            request.AddParameter("date", formatDate);
+            var response = client.Execute<Currency>(request);
+            var inputString = response.Content.Replace("<?xml version=\"1.0\" encoding=\"windows-1251\"?>", "");
+            XmlSerializer serializer = new XmlSerializer(typeof(Courses));
+            var result = new Courses();
+            using (TextReader reader = new StringReader(inputString))
+            {
+                result = (Courses)serializer.Deserialize(reader);
+            }
+            return result;
+        }
+
+    }
+
+    [XmlRoot(ElementName = "currency")]
+    public class Currency
+    {
+        [XmlAttribute(AttributeName = "code")]
+        public string Code { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+        [XmlElement(ElementName = "currencySell")]
+        public string CurrencySell { get; set; }
+        [XmlElement(ElementName = "currencyBuy")]
+        public string CurrencyBuy { get; set; }
+    }
+
+    [XmlRoot(ElementName = "course")]
+    public class Course
+    {
+        [XmlElement(ElementName = "currency")]
+        public List<Currency> Currency { get; set; }
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+        [XmlAttribute(AttributeName = "date")]
+        public string Date { get; set; }
+    }
+
+    [XmlRoot(ElementName = "courses")]
+    public class Courses
+    {
+        [XmlElement(ElementName = "course")]
+        public List<Course> Course { get; set; }
+        [XmlAttribute(AttributeName = "bank")]
+        public string Bank { get; set; }
+        [XmlAttribute(AttributeName = "url")]
+        public string Url { get; set; }
+    }
+}
